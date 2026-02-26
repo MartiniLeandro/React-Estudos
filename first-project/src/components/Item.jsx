61b94ee63140a860c0ff0ba58bcdf5aaf8d4e610
Leandro Martini <martinileandro12@gmail.com>
2026-02-26 10:01:21 -0300

    PropTypes e DefaultProps (modo antigo, sem TS)

--- first-project/src/components/Item.jsx
+++ first-project/src/components/Item.jsx
@@ -1,9 +1,21 @@
-function Item({item}){
+import PropTypes from "prop-types";
+
+function Item({marca, ano_lancamento}){
     return(
         <>
-            <li>{item}</li>
+            <li>{marca} - {ano_lancamento}</li>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 0
 }
 
 export default Item;
--- first-project/src/components/List.jsx
+++ first-project/src/components/List.jsx
@@ -5,10 +5,9 @@ function List(){
         <>
         <h1>Minha Lista</h1>
         <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <Item item="Item 3"/>
-            <Item item="Item 4"/>
+            <Item marca="Ferrari" ano_lancamento={1985}/>
+            <Item marca="Ferrari" ano_lancamento={1985}/>
+            <Item marca="Ferrari" ano_lancamento={1985}/>
         </ul>
         </>
     )
