f1335a624f5fe3b138399cfe738210f31f08f526
Leandro Martini <martinileandro12@gmail.com>
2026-03-18 21:40:40 -0300

    conhecendo o useRef

--- firstProject-udemy/src/components/ListasFormularios.tsx
+++ firstProject-udemy/src/components/ListasFormularios.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 
 function ListasFormularios(){
-    const [tasksList, setTasksList] = useState<string[]>([])
-    const [taskInput, setTaskInput] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null);
+    const firstRender = useRef(true); 
+    const [tasksList, setTasksList] = useState<string[]>([]);
+    const [taskInput, setTaskInput] = useState("");
 
     useEffect(() => {
         const tasksSalvas = localStorage.getItem("@cursoreact")
@@ -14,6 +16,14 @@ function ListasFormularios(){
     
     }, [])
 
+    useEffect(() => { 
+        if(firstRender.current){
+            firstRender.current = false
+            return;
+        }
+        localStorage.setItem("@cursoreact", JSON.stringify(tasksList))
+    }, [tasksList])
+
     function adicionarTarefa(){
         if(!taskInput){
             alert("Preencha o nome da sua tarefa")
@@ -21,25 +31,30 @@ function ListasFormularios(){
         }
         setTasksList([...tasksList, taskInput])
         setTaskInput("")
-        localStorage.setItem("@cursoreact", JSON.stringify([...tasksList, taskInput]))
     }
 
     function deletarTarefa(item:string){
         const novoArray = tasksList.filter((task) => task !==  item)
         setTasksList(novoArray)
-        localStorage.setItem("@cursoreact",JSON.stringify(novoArray))
+    }
+
+    function editarTask(){
+        if(inputRef.current){
+            inputRef.current.placeholder = "Editar tarefa"
+            inputRef.current.focus()
+        }
     }
 
     return(
         <div>
             <h1>Lista de tarefas</h1>
-            <input type="text" placeholder="adicionar tarefa" value={taskInput} onChange={e => setTaskInput(e.target.value)}/>
+            <input type="text" placeholder="adicionar tarefa" value={taskInput} onChange={e => setTaskInput(e.target.value)} ref={inputRef}/>
             <button onClick={adicionarTarefa}>adicionar</button>
             <hr />
             {tasksList.map((task,index) => (
                 <section key={index}>
                     <span>{index + 1}- {task}</span>
-                    <button>Editar</button>
+                    <button onClick={editarTask}>Editar</button>
                     <button onClick={() => deletarTarefa(task)}>Deletar</button>
                 </section>
             ))}
