bfa618cffdbecbd53978db26b37d5f62300f3ac9
Leandro Martini <martinileandro12@gmail.com>
2026-03-25 19:44:50 -0300

    projeto do curso quase pronto, sem visualizar a resolução

--- projeto-frase/src/App.tsx
+++ projeto-frase/src/App.tsx
@@ -1,120 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [frasesCategoria, setFrasesCategoria] = useState<string[]>([]);
+  const [frase, setFrase] = useState<string>();
+
+  interface frases {
+    id:number,
+    nome:string,
+    frases:string[]
+  }
+
+  
+
+  const allFrases:frases[] = [
+    {
+      id: 1,
+      nome: "Motivação",
+      frases: [
+        "Siga os bons e aprenda com eles.",
+        "O bom-senso vale mais do que muito conhecimento.",
+        "O riso é a menor distância entre duas pessoas.",
+        "Deixe de lado as preocupações e seja feliz.",
+        "Realize o óbvio, pense no improvável e conquiste o impossível.",
+        "Acredite em milagres, mas não dependa deles.",
+        "A maior barreira para o sucesso é o medo do fracasso."
+      ]
+    },
+    {
+      id: 2,
+      nome: "Bom dia",
+      frases: [
+        "Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!",
+        "A melhor forma de acordar é pular da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!",
+        "Escreva em seu coração: todo dia é o melhor dia do ano.",
+        "Bom dia! Não se esqueça que a sua alma é o reflexo do sol, tão forte e brilhante quanto um girassol."
+      ]
+    }
+  ]
+
+  function setCategoria(frases:string[]){
+    setFrasesCategoria(frases);
+    console.log(frasesCategoria)
+  }
+
+  function mostrarFrase(){
+    let fraseEscolhida = ''
+    do{
+      const indiceAleatorio = Math.floor(Math.random() * frasesCategoria.length);
+      fraseEscolhida = frasesCategoria[indiceAleatorio];
+    }while(fraseEscolhida == frase)
+    setFrase(fraseEscolhida);
+    console.log(frase)  
+  }
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <main className='main'> 
+      <div className="container">
+        <img src={logo} alt="" className='img'/>
+        <h2 className='title'>Categorias</h2>
+        <div className="btns">
+          {allFrases.map((item) => (
+            <button key={item.id} className='btn-category' onClick={() => setCategoria(item.frases)}>{item.nome}</button>
+          ))}
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <button className='btn-frase' onClick={() => mostrarFrase()}>Gerar Frase</button>
+        <div className="content">
+          <p className='frase-content'>"{frase}"</p>
         </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </div>
+    </main>
   )
 }
 
