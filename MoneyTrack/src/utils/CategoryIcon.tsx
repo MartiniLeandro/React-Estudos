c06cd23a070bf36dc2c78fa61cc06718eeea400f
Leandro Martini <martinileandro12@gmail.com>
2026-05-23 13:37:24 -0300

    adicionando ícones para todas categorias

--- MoneyTrack/src/utils/CategoryIcon.tsx
+++ MoneyTrack/src/utils/CategoryIcon.tsx
@@ -1,4 +1,4 @@
-import { Bitcoin, Car, ChartLine, Gift,  HandCoins, ShoppingCart } from "lucide-react";
+import { BadgeCheck, BadgeX, Balloon, BanknoteArrowDown, Bitcoin, BookCheck, BottleWine, Car, ChartLine, Coffee, CreditCard, Dumbbell, Gift,  GraduationCap,  HandCoins, HeartPulse, House, ShoppingCart, Users } from "lucide-react";
 
 export const categoryIcons: Record<string,any> = {
     "hand-coins": HandCoins,
@@ -6,6 +6,18 @@ export const categoryIcons: Record<string,any> = {
     "gift": Gift,
     "shopping-cart": ShoppingCart,
     "chart-line": ChartLine,
-    "bitcoin": Bitcoin
-
+    "bitcoin": Bitcoin,
+    "dumbbell": Dumbbell,
+    "heart-pulse": HeartPulse,
+    "balloon": Balloon,
+    "house": House,
+    "users": Users,
+    "graduation-cap": GraduationCap,
+    "credit-card": CreditCard,
+    "book-check": BookCheck,
+    "banknote-arrow-down": BanknoteArrowDown,
+    "bottle-wine": BottleWine,
+    "badge-x": BadgeX,
+    "badge-check": BadgeCheck,
+    "coffee": Coffee
 }
