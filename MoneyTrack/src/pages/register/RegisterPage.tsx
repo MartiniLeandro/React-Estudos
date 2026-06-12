26ad7e82291183973e6d50ca41e07dc7b43de2b6
Leandro Martini <martinileandro12@gmail.com>
2026-06-12 06:44:29 -0300

    lógica de cadastro criada e funcionando corretamente

--- MoneyTrack/src/pages/register/RegisterPage.tsx
+++ MoneyTrack/src/pages/register/RegisterPage.tsx
@@ -1,9 +1,24 @@
 import { ChartColumn, Fingerprint, LockKeyhole, Mail, ShieldCheck, User, Users } from "lucide-react";
 import AuthLayout from "../../components/AuthLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import GoogleIcon from "../../components/GoogleIcon";
+import { useState, type SubmitEvent } from "react";
+import api from "../../services/Api";
+import toast from "react-hot-toast";
+
+interface registerData {
+    name: string,
+    cpf: string,
+    email: string,
+    password: string
+}
 
 export default function Register(){
+    const [name,setName] = useState("");
+    const [cpf,setCpf] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate()
 
     const features = [
         {
@@ -23,6 +38,40 @@ export default function Register(){
         },
     ]
 
+    async function registerUser(e: SubmitEvent){
+        e.preventDefault();
+        const dadosRegister:registerData = {name,cpf,email,password}
+
+        if(!dadosRegister.name || dadosRegister.name.trim() === ""){
+            toast.error("Por favor, adicione o seu nome")
+            return;
+        }
+        if(!dadosRegister.cpf || dadosRegister.cpf.trim() === ""){
+            toast.error("Por favor, adicione o seu cpf")
+            return;
+        }
+        if(!dadosRegister.email || dadosRegister.email.trim() === ""){
+            toast.error("Por favor, adicione o seu email")
+            return;
+        }
+        if(!dadosRegister.password || dadosRegister.password.trim() === ""){
+            toast.error("Por favor, adicione a sua senha")
+            return;
+        }
+
+        try{
+            await api.post("/authentication/register", dadosRegister)
+            toast.success("Usuário cadastrado com sucesso")
+            navigate("/login")
+        }catch(error:any){
+            if(error.response?.data?.message){
+                toast.error(error.response.data.message)
+           }else{
+            toast.error("Erro interno. Por favor, tente novamente mais tarde")
+           }
+        }
+    }
+
     return(
         <AuthLayout 
             title={<h2>Crie a sua conta e <span className="text-green-500">comece agora!</span></h2>}
@@ -34,20 +83,20 @@ export default function Register(){
                 <p className="text-gray-400 mb-6">Preencha seus dados para criar sua conta no <span className="text-green-500">MoneyTrack</span></p>
             </div>
 
-            <form className="space-y-5">
+            <form className="space-y-5" onSubmit={registerUser}>
                 <div className="grid grid-cols-2 gap-4">
                     <div className="flex flex-col gap-1">
                         <label className="text-sm text-gray-400">Nome</label>
                         <div className="flex items-center gap-3 bg-[#0B0F14] border border-white/10 rounded-lg px-4 py-3 focus-within:ring-2 focus-within:ring-green-500 transition">
                             <User className="text-gray-500 w-5 h-5"/>
-                            <input type="text" placeholder="Seu nome completo" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600"/>
+                            <input type="text" placeholder="Seu nome completo" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600" value={name} onChange={(e) => setName(e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex flex-col gap-1">
                         <label className="text-sm text-gray-400">CPF</label>
                         <div className="flex items-center gap-3 bg-[#0B0F14] border border-white/10 rounded-lg px-4 py-3 focus-within:ring-2 focus-within:ring-green-500 transition">
                             <Fingerprint className="text-gray-500 w-5 h-5"/>
-                            <input type="text" placeholder="000.000.000-00" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600"/>
+                            <input type="text" placeholder="000.000.000-00" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600" value={cpf} onChange={(e) => setCpf(e.target.value)}/>
                         </div>
                     </div>
                 </div>
@@ -57,14 +106,14 @@ export default function Register(){
                         <label className="text-sm text-gray-400">Email</label>
                         <div className="flex items-center gap-3 bg-[#0B0F14] border border-white/10 rounded-lg px-4 py-3 focus-within:ring-2 focus-within:ring-green-500 transition">
                             <Mail className="text-gray-500 w-5 h-5"/>
-                            <input type="email" placeholder="seu@email.com" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600"/>
+                            <input type="email" placeholder="seu@email.com" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex flex-col gap-1">
                         <label className="text-sm text-gray-400">Senha</label>
                         <div className="flex items-center gap-3 bg-[#0B0F14] border border-white/10 rounded-lg px-4 py-3 focus-within:ring-2 focus-within:ring-green-500 transition">
                             <LockKeyhole className="text-gray-500 w-5 h-5"/>
-                            <input type="password" placeholder="******" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600"/>
+                            <input type="password" placeholder="******" className="bg-transparent outline-none w-full text-white placeholder:text-gray-600" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                     </div>
                 </div>
