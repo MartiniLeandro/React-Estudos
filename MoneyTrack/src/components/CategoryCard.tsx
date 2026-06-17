5a811157e8bca8eabbdb5c7af2c56669d10fef90
Leandro Martini <martinileandro12@gmail.com>
2026-06-17 19:19:52 -0300

    continuando a estilização da tela de categorias e consumindo os dados do backend

--- MoneyTrack/src/components/CategoryCard.tsx
+++ MoneyTrack/src/components/CategoryCard.tsx
@@ -1,6 +1,12 @@
 import { Info, Tag } from "lucide-react";
 
-export default function CategoryCard(){
+interface Props {
+  totalQuantity?: number,
+  expenseQuantity?: number,
+  revenueQuantity?: number;
+}
+
+export default function CategoryCard({totalQuantity, expenseQuantity, revenueQuantity}: Props){
     return(
         <div className="bg-[#121821] rounded-xl border border-white/10 px-4 py-4">
         <div className="flex justify-between items-center">
@@ -10,10 +16,9 @@ export default function CategoryCard(){
             </div>
             <div>
               <p className="text-sm text-gray-400">Total de categorias</p>
-              <h2 className="text-3xl font-bold">18</h2>
-              <p className="text-sm text-gray-400">
-                7 receitas • 11 despesas
-              </p>
+              <h2 className="text-3xl font-bold">{totalQuantity}</h2>
+              <span className="text-sm text-gray-400 mr-2.5">• {revenueQuantity} receitas</span>
+              <span className="text-sm text-gray-400">• {expenseQuantity} despesas</span>
             </div>
           </div>
           <div className="bg-white/2 border border-white/10 rounded-xl p-4 max-w-md">
--- MoneyTrack/src/pages/categorias/CategoriasPage.tsx
+++ MoneyTrack/src/pages/categorias/CategoriasPage.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
 import CategoryCard from "../../components/CategoryCard"
 import Sidebar from "../../components/Sidebar"
+import api from "../../services/Api";
+import toast from "react-hot-toast";
+
+
+interface CategoriesData {
+    categories: Category[],
+    TotalQuantity: number,
+    ExpenseQuantity: number,
+    RevenueQuantity: number
+}
+
+interface Category{
+    id: number,
+    name: string,
+    typeValue: string,
+    icon: string,
+    color: string
+}
 
 export default function Categorias(){
+    const [categories, setCategories] = useState<Category[]>([])
+    const [totalQuantity,setTotalQuantity] = useState<number>();
+    const [expenseQuantity,setExpenseQuantity] = useState<number>();
+    const [revenueQuantity,setRevenueQuantity] = useState<number>();
+
+    useEffect(() => {
+        getCategoriesData()
+    }, [])
+
+    async function getCategoriesData(){
+        const token = localStorage.getItem("token");
+        try{
+            const response = await api.get<CategoriesData>("/categories/data", {headers: {Authorization: `Bearer ${token}`}})
+            const data = response.data;
+            setCategories(data.categories);
+            setTotalQuantity(data.TotalQuantity);
+            setExpenseQuantity(data.ExpenseQuantity);
+            setRevenueQuantity(data.RevenueQuantity);
+        }catch(error:any){
+            if(error.response?.data?.message){
+                toast.error(error.response.data.message)
+           }else{
+            toast.error("Erro interno. Por favor, tente novamente mais tarde")
+           }
+        }
+    }
+
     return(
         <div className="flex w-full h-screen">
 
@@ -23,15 +69,44 @@ export default function Categorias(){
                 </header>
 
                 {/*CARD*/}
-                <CategoryCard/>
+                <CategoryCard totalQuantity={totalQuantity} expenseQuantity={expenseQuantity} revenueQuantity={revenueQuantity}/>
 
                 {/*FILTROS*/}
                 <div className="flex justify-between mt-3.5">
-                    <input type="text" placeholder="Buscar categoria..."/>
+                    <input type="text" placeholder="Buscar categoria..." className="border border-white/20"/>
                     <select>
                         <option value="">Todas as opções</option>
                     </select>
                 </div>
+
+                {/*TABELA*/}
+                <div className="bg-[#121821] border border-white/10 rounded-xl overflow-hidden mt-6">
+                    <table className="w-full mt-3 table-fixed">
+                        <thead>
+                        <tr>
+                            <th className="w-[30%] py-4 pl-4">Categoria</th>
+                            <th className="w-[20%]">Tipo</th>
+                            <th className="w-[20%]">Cor</th>
+                            <th className="w-[25%]">Ícone</th>
+                            <th className="w-[10%]">Ações</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="border border-white/5 hover:bg-white/10 transition">
+                                {categories.map(category => (
+                                    <div key={category.id}>
+                                        <td>{category.name}</td>
+                                        <td>{category.typeValue}</td>
+                                        <td>{category.color}</td>
+                                        <td>{category.icon}</td>
+                                        <td>ações</td>
+                                    </div>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            
 
             </div>
         </div>
