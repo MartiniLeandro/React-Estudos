438c60d02ee96c76ba82b1357daafa31046295e1
Leandro Martini <martinileandro12@gmail.com>
2026-06-20 19:59:13 -0300

    resolvendo erro no backend e estilizando mais um pouco a tela das categorias

--- MoneyTrack/src/pages/categorias/CategoriasPage.tsx
+++ MoneyTrack/src/pages/categorias/CategoriasPage.tsx
@@ -3,7 +3,7 @@ import CategoryCard from "../../components/CategoryCard"
 import Sidebar from "../../components/Sidebar"
 import api from "../../services/Api";
 import toast from "react-hot-toast";
-import { icons } from "lucide-react";
+import { icons, Lock, Pencil, Search, Trash2 } from "lucide-react";
 
 
 interface CategoriesData {
@@ -18,7 +18,8 @@ interface Category{
     name: string,
     typeValue: string,
     icon: string,
-    color: string
+    color: string,
+    userId: number
 }
 
 export default function Categorias(){
@@ -50,9 +51,11 @@ export default function Categorias(){
     }
 
     function colorCategory(color: string) {
-        return (
+
+        if(color != null) return (
             <div className="w-5 h-5 rounded-full shadow-sm" style={{backgroundColor: `${color}`}}></div>
         )
+        return <span className="text-gray-500">?</span>;
     }
 
     function formatIconName(dbName: string) {
@@ -101,8 +104,12 @@ export default function Categorias(){
 
                 {/*FILTROS*/}
                 <div className="flex justify-between mt-3.5">
-                    <input type="text" placeholder="Buscar categoria..." className="border border-white/20"/>
-                    <select>
+                    <div className="flex items-end justify-end bg-gray-950 p-1 rounded-md border border-white/10">
+                        <input type="text" placeholder="Buscar lançamento"/>
+                        <Search/>
+                    </div>              
+
+                    <select className="">
                         <option value="">Todas as opções</option>
                     </select>
                 </div>
@@ -115,18 +122,25 @@ export default function Categorias(){
                             <th className="w-[30%] py-4 pl-4">Categoria</th>
                             <th className="w-[20%]">Tipo</th>
                             <th className="w-[20%]">Cor</th>
-                            <th className="w-[25%]">Ícone</th>
-                            <th className="w-[10%]">Ações</th>
+                            <th className="w-[20%]">Ícone</th>
+                            <th className="w-[15%] p-16">Ações</th>
                         </tr>
                         </thead>
                         <tbody>
                             {categories.map(category => (
                                 <tr className="border border-white/5 hover:bg-white/10 transition" key={category.id}>
                                     <td className="py-4 pl-4">{category.name}</td>
-                                    <td>{category.typeValue}</td>
+                                    {category.typeValue == 'REVENUE' ? <td><span className="bg-green-950 p-1 text-green-400 rounded-md">Receita</span></td> : 
+                                    <td><span className="bg-red-950 p-1 text-red-400 rounded-md">Despesa</span></td>}
                                     <td>{colorCategory(category.color)}</td>
                                     <td>{renderIcon(category.icon, category.color)}</td>
-                                    <td>ações</td>
+                                    {category.userId == null ? <td><div className="bg-white/6 border border-white/10 rounded-xl p-2 flex w-fit gap-1 text-[16px] items-center"><Lock width={20}/> Padrão do sistema</div></td> : 
+                                    <td>
+                                        <div className="flex gap-2.5 pl-9">
+                                            <div className="bg-white/6 rounded-xl p-2"><Pencil className="text-gray-200 cursor-pointer"/></div>
+                                            <div className="bg-white/6 rounded-xl p-2"><Trash2 className="cursor-pointer text-red-500"/></div>
+                                        </div>
+                                    </td>}
                                 </tr>
                             ))} 
 
