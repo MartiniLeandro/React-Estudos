8b2e6c0b8b4c295ebd76f36bc2cddc368bf0e2cf
Leandro Martini <martinileandro12@gmail.com>
2026-06-27 19:52:22 -0300

    implementando estilização na parte de profile da sidebar

--- MoneyTrack/src/components/Sidebar.tsx
+++ MoneyTrack/src/components/Sidebar.tsx
@@ -1,18 +1,33 @@
 import logo  from '../../public/logo (2).png'
-import { Link } from "react-router-dom";
-import { House, ReceiptText, ChartPie } from 'lucide-react';
+import { Link, useNavigate } from "react-router-dom";
+import { House, ReceiptText, ChartPie, CircleUser, ChevronDown, LogOut, ChevronUp} from 'lucide-react';
 import { useProfileData } from '../hooks/useProfileData';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Sidebar(){
   const {getProfileData, name, email } = useProfileData();
+  const [profileInfo, setProfileInfo] = useState<boolean>(false)
+  const navigate = useNavigate();
 
   useEffect(() => {
     getProfileData();
   }, [])
+
+  function showExitProfile(){
+    if(profileInfo){
+      setProfileInfo(false)
+    }else{
+      setProfileInfo(true)
+    }
+  }
+
+  function exit(){
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
   
     return(
-        <div className="w-52 bg-[#0C131C] border-r border-white/5 flex flex-col justify-between"> 
+        <div className="w-60 bg-[#0C131C] border-r border-white/5 flex flex-col justify-between"> 
           <div className="flex flex-col mt-5">
             <Link to="/"><img src={logo} className='mb-5'/></Link>
             <div className="p-1.5 m-1 rounded-b-sm flex gap-1.5 mx-5"><House/> <Link to="/">Início</Link></div>
@@ -20,8 +35,20 @@ export default function Sidebar(){
             <div className="p-1.5 m-1 rounded-b-sm flex gap-1.5 mx-5"><ChartPie/> <Link to="/categories">Categorias</Link></div>
           </div>
           <div>
-            <div className="bg-[#121821] border border-white/10 mb-6 mx-2.5 p-3 rounded-xl">
-              <p className='text-center'>{name} <br/> {email}</p>
+            {profileInfo && (
+              <div className='border-t border-white/5 p-5 text-center cursor-pointer'>
+                <button className='text-red-600 cursor-pointer' onClick={exit}><LogOut className='inline'/> Sair da conta</button>
+              </div>
+            )}
+            <div className="border-t border-white/5 p-5 flex justify-center items-center cursor-pointer" onClick={showExitProfile}>
+              <div>
+                <CircleUser size={34} className=' mr-2'/>
+              </div>
+              <div>
+                <p>{name}</p>
+                <p>{email}</p>
+              </div>
+              <button className='ml-2 cursor-pointer'>{profileInfo ? <ChevronUp/> : <ChevronDown/>}</button>
             </div>
           </div>
         </div>
